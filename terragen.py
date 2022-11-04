47c07ad34e7fcf16afecfa30045fecb26ee3e3ec
lemogne <maciejkwas@poczta.fm>
2022-11-04 18:52:32 +0100

    Moves three functions to their relevant files

--- terragen.py
+++ terragen.py
@@ -1,5 +1,32 @@
 from init import *
 
+def generate_perlin_noise_2d(shape, res, seed=None):
+	if seed == None:
+		seed = World.seed
+
+	def f(t):
+		return 6 * t**5 - 15 * t**4 + 10 * t**3
+
+	delta = (res[0] / shape[0], res[1] / shape[1])
+	d = (shape[0] // res[0], shape[1] // res[1])
+	grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+	# Gradients
+	angles = 2 * np.pi * rand(seed, (res[0] + 1, res[1] + 1))
+	gradients = np.dstack((np.cos(angles), np.sin(angles)))
+	g00 = gradients[0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+	g10 = gradients[1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+	g01 = gradients[0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1)
+	g11 = gradients[1:, 1:].repeat(d[0], 0).repeat(d[1], 1)
+	# Ramps
+	n00 = np.sum(grid * g00, 2)
+	n10 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1])) * g10, 2)
+	n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1] - 1)) * g01, 2)
+	n11 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1] - 1)) * g11, 2)
+	# Interpolation
+	t = f(grid)
+	n0 = n00 * (1 - t[:, :, 0]) + t[:, :, 0] * n10
+	n1 = n01 * (1 - t[:, :, 0]) + t[:, :, 0] * n11
+	return np.sqrt(2) * ((1 - t[:, :, 1]) * n0 + t[:, :, 1] * n1)
 
 # Generate random array of chunks
 def gen_terrain():
