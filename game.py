9884cc906656e6e06f651fe907ccb9c8456b3360
lemogne <87395736+lemogne@users.noreply.github.com>
2024-11-15 13:00:05 +0100

    UI improvements

--- game.py
+++ game.py
@@ -82,80 +82,82 @@ while 1:
 
 		if UI.paused:
 			UI.check_hover(mouse_pos)
-		if UI.buttons.is_typing():
-			if UI.buttons.get_input_button() == None:
-				chat_string = UI.input_text(chat_string, start=2)
-				if not UI.buttons.is_typing():
-					try:
-						exec(chat_string[2:])
-					except Exception as e:
-						exception = f"{type(e).__name__}: {e}"
-						chat_string += "\n" + exception
-						print(exception)
-			else:
-				UI.buttons.get_input_button().run()
-				if not UI.buttons.is_typing():
-					UI.buttons.set_input_button(None)
-		else:
-			for event in pg.event.get():
-				if event.type == pg.QUIT:
-					pg.quit()
-					quit()
-				elif event.type == pg.MOUSEBUTTONDOWN:
-					if UI.paused:
-						if UI.buttons.get_selected():
-							if event.button == 1:
-								UI.buttons.get_selected().run()
-					else:
-						#Place/Destroy blocks
+		for event in pg.event.get():
+			if UI.buttons.is_typing():
+				if UI.buttons.get_input_button() == None:
+					chat_string = UI.input_text(chat_string, event, start=2)
+					if not UI.buttons.is_typing():
+						try:
+							exec(chat_string[2:])
+						except Exception as e:
+							exception = f"{type(e).__name__}: {e}"
+							chat_string += "\n" + exception
+							print(exception)
+				else:
+					UI.buttons.get_input_button().run(event)
+					if not UI.buttons.is_typing():
+						UI.buttons.set_input_button(None)
+			if event.type == pg.QUIT:
+				pg.quit()
+				quit()
+			elif event.type == pg.MOUSEBUTTONDOWN:
+				if UI.paused:
+					if UI.buttons.get_selected():
 						if event.button == 1:
-							looked_at = get_looked_at()[0]
-							if looked_at is not None:
-								if 8 in [
-								    World.get_block(looked_at + (0, 1, 0)),
-								    World.get_block(looked_at + (1, 0, 0)),
-								    World.get_block(looked_at + (-1, 0, 0)),
-								    World.get_block(looked_at + (0, 0, 1)),
-								    World.get_block(looked_at + (0, 0, -1))
-								]:
-									World.set_block(looked_at, 8)
-								else:
-									World.set_block(looked_at, 0)
-						elif event.button == 2:
-							if (looked_at := get_looked_at()[0]) is not None:
-								settings.current_block = World.get_block(looked_at)
-						elif event.button == 3:
-							looked_at = get_looked_at()[1]
-							if looked_at is not None and player.not_in_hitbox(looked_at):
-								World.set_block(looked_at, settings.current_block)
-				elif event.type == pg.VIDEORESIZE and settings.resizeable:
-					Display.init((event.w, event.h))
-					Textures.update_pixel_size()
-				elif event.type == pg.KEYDOWN and not UI.buttons.is_typing():
-					if event.key == pg.K_F2:
-						screenshot()
-					elif event.key == pg.K_F1:
-						settings.shown ^= True
-					elif event.key == pg.K_ESCAPE:
-						toggle_menu()
-					elif event.key == pg.K_m:
-						if UI.show_game_info:
-							chat_string = ""
-						UI.show_game_info ^= True
-					elif event.key == pg.K_f:
-						player.flying ^= True
-					elif event.key == pg.K_r:
-						UI.buttons.set_typing(True)
-						UI.show_game_info = False
-						chat_string = "> _"
-					elif event.key == pg.K_c:
+							UI.buttons.get_selected().run(event)
+					else:
+						UI.buttons.set_typing(False)
+						UI.buttons.set_input_button(None)
+				else:
+					#Place/Destroy blocks
+					if event.button == 1:
+						looked_at = get_looked_at()[0]
+						if looked_at is not None:
+							if 8 in [
+								World.get_block(looked_at + (0, 1, 0)),
+								World.get_block(looked_at + (1, 0, 0)),
+								World.get_block(looked_at + (-1, 0, 0)),
+								World.get_block(looked_at + (0, 0, 1)),
+								World.get_block(looked_at + (0, 0, -1))
+							]:
+								World.set_block(looked_at, 8)
+							else:
+								World.set_block(looked_at, 0)
+					elif event.button == 2:
+						if (looked_at := get_looked_at()[0]) is not None:
+							settings.current_block = World.get_block(looked_at)
+					elif event.button == 3:
+						looked_at = get_looked_at()[1]
+						if looked_at is not None and player.not_in_hitbox(looked_at):
+							World.set_block(looked_at, settings.current_block)
+			elif event.type == pg.VIDEORESIZE and settings.resizeable:
+				Display.init((event.w, event.h))
+				Textures.update_pixel_size()
+			elif event.type == pg.KEYDOWN and not UI.buttons.is_typing():
+				if event.key == pg.K_F2:
+					screenshot()
+				elif event.key == pg.K_F1:
+					settings.shown ^= True
+				elif event.key == pg.K_ESCAPE:
+					toggle_menu(event)
+				elif event.key == pg.K_m:
+					if UI.show_game_info:
 						chat_string = ""
-					elif event.key == pg.K_UP:
-						settings.current_block += 1
-						settings.current_block %= len(game_blocks)
-					elif event.key == pg.K_DOWN:
-						settings.current_block -= 1
-						settings.current_block %= len(game_blocks)
+					UI.show_game_info ^= True
+				elif event.key == pg.K_f:
+					player.flying ^= True
+				elif event.key == pg.K_r:
+					UI.buttons.set_typing(True)
+					UI.show_game_info = False
+					chat_string = "> _"
+				elif event.key == pg.K_c:
+					chat_string = ""
+				elif event.key == pg.K_UP:
+					settings.current_block += 1
+					settings.current_block %= len(game_blocks)
+				elif event.key == pg.K_DOWN:
+					settings.current_block -= 1
+					settings.current_block %= len(game_blocks)
 	UI.in_menu = True
 	chunk_loop.join()
 	if world_infinite:
